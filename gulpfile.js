e356f6c44eea6334fb10eb17645133c7cd23d8f4
Emil Gedda <emil.gedda@emilgedda.se>
2016-04-05 23:24:29 +0200

    Gulp copies vendor-files

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var gulp = require('gulp'),
     minifyCSS = require('gulp-clean-css'),
     jshint = require('gulp-jshint');
 
-gulp.task('default', ['copy', 'css', 'js']); 
+gulp.task('default', ['copy','copyvendor', 'css', 'js']); 
 
 gulp.task('browserSync', function() {
     browserSync.init({
@@ -17,6 +17,7 @@ gulp.task('browserSync', function() {
         },
     })
 })
+
 gulp.task('copy', function() {
     return gulp.src('app/*.html')
         .pipe(gulp.dest('dist'))
@@ -24,6 +25,19 @@ gulp.task('copy', function() {
             stream: true
     }))
 });
+
+gulp.task('copyvendor', function() {
+    return gulp.src(['bower_components/angular/angular.min.js', 
+                    'bower_components/angular/angular.min.js.map',
+                    'bower_components/bootstrap/dist/css/bootstrap.min*'])
+        .pipe(gulp.dest('dist/vendor'))
+        .pipe(browserSync.reload({
+            stream: true
+    }))
+});
+
+
+
 gulp.task('lint', function() {
     return gulp.src('app/**/*.js')
         .pipe(jshint())
@@ -31,7 +45,7 @@ gulp.task('lint', function() {
         .pipe(jshint.reporter('fail'))
 });
 
-gulp.task('watch', ['browserSync', 'css', 'lint', 'copy'],  function(){
+gulp.task('watch', ['browserSync', 'default'],  function(){
     gulp.watch('assets/scss/**/*.scss', ['css']);
     gulp.watch('app/**/*.js', ['lint', 'js']);
     gulp.watch('app/*.html', ['copy']);
