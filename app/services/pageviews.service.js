ccaf661d3e6c5aab9e53e2652c142c5c5c20ec15
Emil Gedda <emil.gedda@emilgedda.se>
2016-04-14 01:02:13 +0200

    Added linebreaks to URL

--- app/services/pageviews.service.js
+++ app/services/pageviews.service.js
@@ -1,5 +1,6 @@
 module.exports = function($resource) {
-    var URL = 'https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/:project/all-access/user/:article/daily/:from/:to';
+    var URL = 'https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/' +
+               ':project/all-access/user/:article/daily/:from/:to';
     return $resource(URL, {}, {
         query: {method: 'GET'}
     });
