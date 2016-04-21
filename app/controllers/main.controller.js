f1310a018e21feb2f0f2640df841dc1c2cc61450
Anders Eriksson <aeri3@kth.se>
2016-04-21 17:09:47 +0200

    Added bottom column with datepickers for from and end dates.

--- app/controllers/main.controller.js
+++ app/controllers/main.controller.js
@@ -78,6 +78,74 @@ module.exports = function($scope, pageViews, searchService) {
     }).$promise.then(function(result) {
         console.log(result.article);
     });
+    
+    // DATEPICKER
+    // TODO: Move everything related to bottom bar date pickers
+    // to separate controller
+    $scope.today = function() {
+        $scope.dt = new Date();
+    };
+    $scope.today();
 
+    $scope.clear = function() {
+        $scope.dt = null;
+    };
+
+    $scope.inlineOptions = {
+        customClass: getDayClass,
+        minDate: new Date(),
+        showWeeks: true
+    };
+
+    $scope.dateOptions = {
+        formatYear: 'yy',
+        maxDate: new Date(),
+        minDate: new Date(),
+        startingDay: 1
+    };
+
+    $scope.toggleMin = function() {
+        $scope.inlineOptions.minDate = $scope.inlineOptions.minDate ? null : new Date();
+        $scope.dateOptions.minDate = $scope.inlineOptions.minDate;
+    };
+
+    $scope.toggleMin();
+
+    $scope.openFrom = function() {
+        $scope.popupFrom.opened = true;
+    };
+
+    $scope.openTo = function() {
+        $scope.popupTo.opened = true;
+    };
+
+    $scope.setDate = function (year, month, day) {
+        $scope.dt = new Date(year, month, day);
+    };
+
+    $scope.popupFrom = {
+        opened: false
+    };
+
+    $scope.popupTo = {
+        opened: false
+    };
+
+    function getDayClass(data) {
+        var date = data.date,
+            mode = data.mode;
+        if (mode === 'day') {
+            var dayToCheck = new Date(date).setHours(0, 0, 0, 0);
+
+            for (var i = 0; i < $scope.events.length; i++) {
+                var currentDay = new Date($scope.events[i].date).setHours(0, 0, 0, 0);
+
+                if (dayToCheck === currentDay) {
+                    return $scope.events[i].status;
+                }
+            }
+        }
+
+        return '';
+    }
 };
-
